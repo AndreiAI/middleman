16366edff6503cbc94e629614a2fe3a43f2230d3
AndreiAI <andrei.iordache.uk@gmail.com>
2017-10-18 01:00:55 +0100

    Added reCaptcha on sign Up

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -36,23 +36,63 @@ let mailOptions = {
 var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
-            var user = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email.toLowerCase(),
-                password: req.body.password,
-                type: 'client'
-            }
+            //~~~~~~~~~~~~~~~~Check for spambots~~~~~~~~~~~~~~~~~~~~~~~~
+            var userIP = req.connection.remoteAddress;
 
-            database.saveUser(user, function (response) {
-                if (response === true) {
-                    req.login(req.body, function () {
-                        res.redirect(307, '/auth/logIn');
-                    });
-                } else {
-                    res.redirect('/');
-                }
+            var postData = querystring.stringify({
+                'secret': recaptchaSecretKey,
+                'response': req.body['g-recaptcha-response'],
+                'remoteip': userIP
             });
+
+            var options = {
+                hostname: 'google.com',
+                port: 443,
+                path: '/recaptcha/api/siteverify',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': postData.length
+                }
+            };
+
+            var request = https.request(options, (response) => {
+                console.log('statusCode:', response.statusCode);
+                console.log('headers:', response.headers);
+
+                response.on('data', (data) => {
+                    data = JSON.parse(data);
+
+                    if (data.success) {
+                        var user = {
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            email: req.body.email.toLowerCase(),
+                            password: req.body.password,
+                            type: 'client'
+                        }
+
+                        database.saveUser(user, function (response) {
+                            if (response === true) {
+                                req.login(req.body, function () {
+                                    res.redirect(307, '/auth/logIn');
+                                });
+                            } else {
+                                res.redirect('/');
+                            }
+                        });
+                    } else {
+                        //req.logout();
+                        res.redirect('/');
+                    }
+                });
+            });
+
+            request.write(postData);
+            request.end();
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
         });
 
     authRouter.route('/signUpHandler')
