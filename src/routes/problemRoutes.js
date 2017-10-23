ea286c521d116c05ef721097eac059a52a105280
AndreiAI <andrei.iordache.uk@gmail.com>
2017-10-23 20:58:07 +0100

    Test

--- src/routes/problemRoutes.js
+++ src/routes/problemRoutes.js
@@ -84,6 +84,7 @@ var router = function () {
             });
         })
         .post(function (req, res) {
+            console.log(req);
             Number.prototype.padLeft = function (base, chr) {
                 var len = (String(base || 10).length - String(this).length) + 1;
                 return len > 0 ? new Array(len).join(chr || '0') + this : this;
